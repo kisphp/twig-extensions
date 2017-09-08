0f081e8558b0d2bee7d60c7787a18e6174a3b068
Marius Bogdan Rizac <kisphp@users.noreply.github.com>
2017-09-08 23:20:51 +0200

    add usage info (#3)

--- tests/Twig/Helpers/DemoFunction.php
+++ tests/Twig/Helpers/DemoFunction.php
@@ -6,16 +6,25 @@ use Kisphp\Twig\AbstractTwigFunction;
 
 class DemoFunction extends AbstractTwigFunction
 {
+    /**
+     * @return array
+     */
     public function getProvidedOptions()
     {
         return $this->getExtensionOptions();
     }
 
+    /**
+     * @return string
+     */
     protected function getExtensionName()
     {
         return 'demo';
     }
 
+    /**
+     * @return \Closure
+     */
     protected function getExtensionCallback()
     {
         return function ($name) {
--- tests/Twig/Helpers/DemoFunctionSafeHtml.php
+++ tests/Twig/Helpers/DemoFunctionSafeHtml.php
@@ -9,16 +9,25 @@ class DemoFunctionSafeHtml extends AbstractTwigFunction
 {
     use IsSafeHtml;
 
+    /**
+     * @return array
+     */
     public function getProvidedOptions()
     {
         return $this->getExtensionOptions();
     }
 
+    /**
+     * @return string
+     */
     protected function getExtensionName()
     {
         return 'demo';
     }
 
+    /**
+     * @return \Closure
+     */
     protected function getExtensionCallback()
     {
         return function ($name) {
